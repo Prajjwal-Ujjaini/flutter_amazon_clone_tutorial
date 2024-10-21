2c25573f754619b960d2faf388d80814ab40bf30
prajjwal ujjaini <prajjwalujjaini@gmail.com>
2024-10-21 19:08:14 +0530

    auth completed

---
+++ lib/providers/user_provider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../models/user_model.dart';
+
+class UserProvider extends ChangeNotifier {
+  UserModel _user = UserModel(
+    id: '',
+    name: '',
+    email: '',
+    password: '',
+    address: '',
+    type: '',
+    token: '',
+    // cart: [],
+  );
+
+  UserModel get user => _user;
+
+  void setUser(String user) {
+    _user = UserModel.fromJson(user);
+    notifyListeners();
+  }
+
+  void setUserFromMap(Map<String, dynamic> user) {
+    _user = UserModel.fromMap(user);
+    notifyListeners();
+  }
+
+  void setUserFromModel(UserModel user) {
+    _user = user;
+    notifyListeners();
+  }
+}
